e8c982895289c25dd90c1f3ae5c68fbd86cafaa3
Maria Ryattel <mashulya.marycheva@mail.ru>
2026-04-04 17:11:54 +0300

    add new branch with lessonOne

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,46 +4,36 @@ import static org.example.LessonOne.*;
 
 public class Main {
     public static void main(String[] args) {
-        LessonOne method1 = new LessonOne();
-        method1.printThreeWords();
+        LessonOne method = new LessonOne();
+        method.printThreeWords();
 
-        LessonOne method2 = new LessonOne();
-        method2.checkSumSign();
+        method.checkSumSign();
 
-        LessonOne method3 = new LessonOne();
-        method3.printColor();
+        method.printColor();
 
-        LessonOne method4 = new LessonOne();
-        method4.compareNumbers();
+        method.compareNumbers();
 
         System.out.println(checkingAmount(1, 9));
         System.out.println();
 
-        LessonOne method6 = new LessonOne();
-        method6.positiveOrNegative(-1);
+        method.positiveOrNegative(-1);
 
         System.out.println(isNegative(333));
         System.out.println();
 
-        LessonOne method8 = new LessonOne();
-        method8.printStringToNumber("восьмой метод", 3);
+        method.printStringToNumber("восьмой метод", 3);
 
         System.out.println(leapYear(104));
         System.out.println();
 
-        LessonOne method10 = new LessonOne();
-        method10.changeArray();
+        method.changeArray();
 
-        LessonOne method11 = new LessonOne();
-        method11.emptyArray();
+        method.emptyArray();
 
-        LessonOne method12 = new LessonOne();
-        method12.multiplySmallNumbers();
+        method.multiplySmallNumbers();
 
-        LessonOne method13 = new LessonOne();
-        method13.squareArray();
+        method.squareArray();
 
-        LessonOne method14 = new LessonOne();
-        method14.createArray(3, 5);
+        method.createArray(3, 5);
     }
 }
